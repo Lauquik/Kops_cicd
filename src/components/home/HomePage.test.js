a8e415a2df73d17afac9966d3feb354302cd94ce
Lauquik <laukik.22010776@viit.ac.in>
2023-09-01 15:04:25 +0530

    hoempage testcase updated

--- src/components/home/HomePage.test.js
+++ src/components/home/HomePage.test.js
@@ -1,27 +1,14 @@
+import { render, waitFor } from "@testing-library/react";
 import React from "react";
-import { render, screen } from "@testing-library/react";
 import HomePage from "./HomePage";
-import {BrowserRouter as Router} from 'react-router-dom';
 
-const countItems = () => {
-  const items = screen.getAllByRole('link', { name: /item/i });
-  return items.length;
-};
+test("fetches data from /api/items when HomePage is rendered", async () => {
+  const mockFetch = jest.fn(() => Promise.resolve({ json: () => [] }));
+  global.fetch = mockFetch;
 
-// Sample test to verify the number of items
-it('should display the correct number of items', () => {
+  render(<HomePage />);
 
-  // Render the component
-  render(
-    <Router>
-      <HomePage />
-    </Router>
-  );
-  const numberOfItems = countItems();
-  console.log(numberOfItems);
-  // Call the function to count items
-
-
-  // Assert the expected number of items
-  expect(numberOfItems).toBe(10); // Modify the number if needed based on the actual number of items in the document
+  await waitFor(() => {
+    expect(mockFetch).toHaveBeenCalledWith("/api/items");
+  });
 });
